da3532bfb84ce973620d38cad4492df33ef950b5
millergallegof <miller.gallegof@gmail.com>
2023-08-26 08:20:02 -0500

    Se crea un arreglo

---
+++ main.js
@@ -0,0 +1,6 @@
+// ARREGLOS
+const animales = ['perro', 'gato', 'tortuga', 'elefante'];
+const numeros = [1, 2, 3, 4, 50, 100]
+console.log("este es mi arreglo de animales", animales);
+console.log(numeros);
+// agregar otro animal
